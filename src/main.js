3d7580f5485415990d057724d9ed106ed9c68170
zhangyanling <18783226594@163.com>
2020-09-26 12:13:31 +0800

    feat: init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue';
+import * as Sentry from '@sentry/browser';
+import { Vue as VueIntegration } from '@sentry/integrations';
+import { Integrations } from '@sentry/tracing';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+// process.env.NODE_ENV === 'production' && Sentry.init();
+Sentry.init({
+  dsn: 'http://cd90ec30622343bbadafbe77bcbda083@localhost:9000/3',
+  integrations: [
+    new VueIntegration({
+      Vue,
+      tracing: true,
+      attachProps: true,
+    }),
+    new Integrations.BrowserTracing(),
+  ],
+  tracesSampleRate: 1,
+  logErrors: true,
+  release: 'pro@1.0.1',
+});
 
 new Vue({
   router,
